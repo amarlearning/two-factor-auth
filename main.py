9f7a1bf7665247fc1ad86d5a7c094ed073fb7d96
Amar Prakash Pandey <amar.om1994@gmail.com>
2017-06-18 03:40:32 +0530

    main script added

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os, re
+import sys 
+import string
+from clint.textui import colored, puts
+
+
+class TwoFatorAuth():
+
+	""" 
+		docstring for TwoFatorAuth : Generate two-factor authentication (2FA) tokens by 
+		implement the Time-Based One-Time Password (TOTP) algorithm.
+
+	"""
+
+
+
+
+
